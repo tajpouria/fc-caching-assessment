d6404e277ff768ebb400b104a72a0858ae81c4e0
tajpouria.dev@gmail.com <tajpouria.dev@gmail.com>
2021-04-20 20:38:12 +0430

    Added -- Mongo cache util

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { DataDocument } from 'src/schemas/data.schema';
+import { DataService } from './data.service';
 
 @Controller('data')
-export class DataController {}
+export class DataController {
+  constructor(private dataService: DataService) {}
+
+  @Get('/:key')
+  getDataByKey(@Param('id') key: string): Promise<DataDocument> {
+    console.info('hello');
+    return this.dataService.getDataByKey(key);
+  }
+}
